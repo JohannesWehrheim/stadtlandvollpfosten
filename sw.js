baf68fb6a3689de27c9a2c8c3a956800997ef40e
JohannesWehrheim <78vdk9ww7w@privaterelay.appleid.com>
2026-07-13 23:39:00 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'slv-cache-v1'; // WICHTIG: Ändere dies zu v2, v3 etc., wenn du MP3s tauschst!
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+  // Später hier './3.mp3' etc. einfügen
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
